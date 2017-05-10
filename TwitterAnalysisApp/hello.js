6f4b84d6565bcf47611f98f2e017c3e5c7590f57
Karl <gkcsjk@gmail.com>
2017-05-11 03:50:42 +1000

    Final Version

--- TwitterAnalysisApp/hello.js
+++ TwitterAnalysisApp/hello.js
@@ -18,6 +18,30 @@ const couch = new NodeCouchDb({
 const dbName = 'emlbourne_ans';
 const viewUrl = '_design/melbourme_view/_view/lang_withcoor';
 
+
+const MelbourneDB = 'emlbourne_ans';
+const MelbourneUrl = '_design/melbourme_view/_view/time_emotion?reduce=true&group_level=3';
+
+const AdelaideDB = 'adelaide_ans';
+const AdelaideUrl = '_design/adelaide_view/_view/time_emotion?reduce=true&group_level=3';
+
+const SydneyDB = 'sydney_ans';
+const SydneyUrl = '_design/sydney_view/_view/time_emotion?reduce=true&group_level=3';
+
+const BrisbaneDB = 'brisbane_ans';
+const BrisbaneUrl = '_design/brisbane_view/_view/time_emotion?reduce=true&group_level=3';
+
+const PerthDB = 'perth_ans';
+const PerthUrl = '_design/perth_view/_view/time_emotion?reduce=true&group_level=3';
+
+const CanberraDB = 'canberra_ans';
+const CanberraUrl = '_design/canberra_view/_view/time_emotion?reduce=true&group_level=3';
+
+const airlineDB = 'result_airline';
+const airlineUrl = '_design/airline/_view/airlineView';
+
+
+
 couch.listDatabases().then(function(dbs){
 	console.log(dbs);
 });
@@ -37,17 +61,44 @@ app.get('/', function(req, res){
 });
 
 app.get('/app1', function(req, res){
-	couch.get(dbName, viewUrl).then(
-		function(data, headers, status){
-			// console.log(data.data.rows);
-			res.render('app1',{
-				emlbourne_ans: data.data.rows
-			});
-		},
-		function(err){
-			res.send(err);
-		});
+	var a = [];
+    
+    //Melbourne
+	couch.get(MelbourneDB, MelbourneUrl).then(function(data, headers, status){
+        a.push( data.data.rows);
+        console.log("melb");
+        
+        couch.get(AdelaideDB, AdelaideUrl).then(function(data, headers, status){
+            a.push( data.data.rows);	
+            console.log("adel");
+            
+            couch.get(BrisbaneDB,BrisbaneUrl).then(function(data,headers,status){
+                a.push(data.data.rows);
+                console.log("bris");
+                
+                couch.get(SydneyDB, SydneyUrl).then(function(data,headers,status){
+                    a.push(data.data.rows);
+                    console.log("syd");
+                    
+                    couch.get(PerthDB,PerthUrl).then(function(data,headers,status){
+                        a.push(data.data.rows);
+                        console.log("pert");
+                        
+                        couch.get(CanberraDB,CanberraUrl).then(function(data,headers,status){
+                            a.push(data.data.rows);
+                            console.log("canb");
+                            
+                            res.render("app1", {a:a});
+                        
+                        },function(err){res.send(err)});
+                    },function(err){res.send(err)});
+                },function(err){res.send(err)});
+            },function(err){res.send(err)});
+        },function(err){res.send(err);});
+    },function(err){res.send(err)});
 });
+
+
 
 app.get('/app2', function(req, res){
 	couch.get(dbName, viewUrl).then(
@@ -63,11 +114,12 @@ app.get('/app2', function(req, res){
 });
 
 app.get('/app3', function(req, res){
-	couch.get(dbName, viewUrl).then(
+	couch.get(airlineDB, "6d60ba402d7b18e556e30b7bf4655a0e").then(
 		function(data, headers, status){
-			// console.log(data.data.rows);
+			console.log(data.data.value);
+			
 			res.render('app3',{
-				emlbourne_ans: data.data.rows
+				airlineDB:data.data.value
 			});
 		},
 		function(err){
